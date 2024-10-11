5a205047aefd8dd3c9ace5254e72761b551dbb5b
Yonesima <SDT2495SC0013@edu.sca.ac.jp>
2024-10-11 11:44:14 +0900

    マウスでの長押し

--- Assets/Mituki/Throwingpower.cs
+++ Assets/Mituki/Throwingpower.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public float Power = 1.0f;
+    public float Power = 0;
     public bool ThowKey = false;
     public float timer = 0;
     // Start is called before the first frame update
@@ -18,15 +18,17 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ThowKey = Input.GetMouseButtonDown(0);
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-            timer += Time.deltaTime;
-                if(Power < 100){
+            Power += 0.01f;
+            Debug.Log(Power + "‘‚¦‚Ä‚é");
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log(Power + "•ú‚µ‚½");
+            Throw();
+            Power = 0;
 
-                Power = timer; 
-                    Throw();
-                }
         }
     }
 
@@ -36,25 +38,25 @@ public class NewBehaviourScript : MonoBehaviour
     public void Throw()
     {
         //—Í‚Ì•ûŒü
-        Vector3 forceDirection = new Vector3(0, Power, Power);
+        Vector3 forceDirection = new Vector3(0, Power / 10, Power / 10);
         float forceMagnitude = 10.0f;
         Vector3 force = forceMagnitude * forceDirection;
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
         rb.AddForce(force, ForceMode.Impulse);
     }
 
-    public void ThrowingPower()
-    {
-        switch (Power)
-        {
-            case 0:
-                if (ThowKey)
-                {
+    //public void ThrowingPower()
+    //{
+    //    switch (Power)
+    //    {
+    //        case 0:
+    //            if (ThowKey)
+    //            {
 
-                }
-            break;
-        }
-    }
+    //            }
+    //        break;
+    //    }
+    //}
 
     public void Gauge()
     {
