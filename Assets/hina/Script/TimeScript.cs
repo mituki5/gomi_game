e9b671f24d87acf52a52b916633a6730e73c2e15
hina <SDT2395GP0017@edu.sca.ac.jp>
2024-10-17 09:42:44 +0900

    UI画面の調整

--- Assets/hina/Script/TimeScript.cs
+++ Assets/hina/Script/TimeScript.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float countdown = 30.0f;
+
+    public Text timeText;
 
     // Update is called once per frame
     void Update()
     {
-        
+        countdown -= Time.deltaTime;
+
+        timeText.text = "Žc‚è" + countdown.ToString("f1") + "•b";
+
+        if(countdown <= 0)
+        {
+            timeText.text = "I—¹I";
+        }
     }
 }
