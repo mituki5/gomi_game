93722bfa1452d86db3365fdbd74a684e691d1b93
hina <sdt2395gp0017@edu.sca.ac.jp>
2024-10-20 18:10:48 +0900

    リザルト実装途中

--- Assets/hina/Script/GaugeScript.cs
+++ Assets/hina/Script/GaugeScript.cs
@@ -8,17 +8,17 @@ public class GaugeScript : MonoBehaviour
 
     public float maxValue = 100f; // ゲージの最大値
     public float currentValue = 0; // 現在の値
+    public float minValue = 0;
 
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            currentValue += 0.1f;
-
-            if (currentValue >= maxValue)
+            if(currentValue == 0)
             {
-                currentValue = 100.0f;
+                GaugeUp();
             }
+
         }
         if (Input.GetMouseButtonUp(0))
         {
@@ -26,5 +26,23 @@ public class GaugeScript : MonoBehaviour
         }
         // ゲージの画像を更新
         gaugeImage1.fillAmount = currentValue / maxValue;
+    }
+
+    private void GaugeUp()
+    {
+        currentValue += 0.1f;
+
+        if (currentValue >= maxValue)
+        {
+            currentValue = 100.0f;
+        }
+    }
+
+    private void GaugeDown()
+    {
+        if(currentValue == maxValue)
+        {
+            currentValue -= 0.1f;
+        }
     }
 }
