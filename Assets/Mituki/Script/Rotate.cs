292e2818e61714a6cae186935d0cc1fc08bd36ec
a <a>
2024-11-17 14:14:26 +0900

    力の調整中

--- Assets/Mituki/Script/Rotate.cs
+++ Assets/Mituki/Script/Rotate.cs
@@ -7,17 +7,6 @@ public class Rotate : MonoBehaviour
     public float _Rotate;
     public float max = 180;
     public float min = -180;
- 
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.A))
-        {
-            //transform.Rotate(0, 0, Random.Range(min,max));
-            Rotatering1();
-        }
-
-    }
-
     //¶‰ñ‚è
     public void Rotateringleft()
     {
@@ -28,19 +17,5 @@ public class Rotate : MonoBehaviour
     public void RotateringRight()
     {
         transform.Rotate(0, 0, -1);
-    }
-
-    //d‚¢‚Æ‚±‚ğ’†S‚É
-    public void Rotatering1()
-    {
-        _Rotate = transform.eulerAngles.z;
-        if (_Rotate <= max)
-        {
-            transform.Rotate(0, 0, -1);
-        }
-        else if(_Rotate >= min)
-        {
-            transform.Rotate(0, 0, 1);
-        }
     }
 }
