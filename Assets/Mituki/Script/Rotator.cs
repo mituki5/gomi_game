91a1609d6b7b9f44ad5832c61cc2b2830ef41478
a <a>
2024-11-21 10:09:13 +0900

    回転の実装

---
+++ Assets/Mituki/Script/Rotator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private float rotateX = 0;
+    [SerializeField]
+    private float rotateY = 0;
+    [SerializeField]
+    private float rotateZ = 0;
+
+    private ThrowingPower throwingPower;
+
+    private void Start()
+    {
+        throwingPower = GetComponent<ThrowingPower>();
+    }
+
+    private void Update()
+    {
+        if(throwingPower.landing == false)
+        {
+            gameObject.transform.Rotate(new Vector3(rotateX,rotateY,rotateZ) * Time.deltaTime);
+        }
+    }
+}
