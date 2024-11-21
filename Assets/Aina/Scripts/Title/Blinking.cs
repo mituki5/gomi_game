97887708ca3c9dad3fc00f31044a2f691d4564e6
aina <sdt2395gp0022@edu.sca.ac,jp>
2024-11-21 12:25:18 +0900

    タイトル画面のスタートがゴミ箱に入るまで生成するスクリプト作成中

--- Assets/Aina/Scripts/Title/Blinking.cs
+++ Assets/Aina/Scripts/Title/Blinking.cs
@@ -13,6 +13,7 @@ public class Blinking : MonoBehaviour
     private GameObject TitleImage;
     private GameObject Titletext;
     private Image image;
+    private GameObject StartBall;
     private float time;
 
     void Start()
@@ -20,15 +21,17 @@ public class Blinking : MonoBehaviour
         Holdtext = this.gameObject.GetComponent<Text>();
         TitleImage = GameObject.Find("TitleNameImage");
         //Titletext = GameObject.Find("TitleNameText");
+        StartBall = GameObject.Find("StartBall");
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             this.gameObject.SetActive(false);
             TitleImage.SetActive(false);
             //Titletext.SetActive(false);
+            StartBall.SetActive(false);
         }
         Holdtext.color = GetAlphaColor(Holdtext.color);
     }
