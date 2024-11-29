b121098a45090858da381cd21833472e14b8b028
minegishi <>
2024-11-29 15:54:17 +0900

    ポーズ画面の配置完了　スプリクトは完成次第付ける

--- Assets/hina/Script/Bottun2.cs
+++ Assets/hina/Script/Bottun2.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 using UnityEngine.SceneManagement;
 
 public class Bottun2 : MonoBehaviour
 {
+    public static float Score;
     public void Title_Bottun()
     {
         SceneManager.LoadScene("Title");
     }
 
-    public void Game_Bottun()
+    public void MainGame_Bottun()
     {
         SceneManager.LoadScene("GameScene 1");
     }
 
-
+    public void REGame_Bottun()
+    {
+        Score = 0;
+        SceneManager.LoadScene("GameScene 1");
+    }
 
 
 }
